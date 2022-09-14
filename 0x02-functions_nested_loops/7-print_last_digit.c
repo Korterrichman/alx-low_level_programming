eb0731a9eeedd09e4c025a358a94c55e4f046be8
Korter Richman <korterrichman@yahoo.com>
2022-09-14 06:08:31 -0700

    add _putchar.c, 7-last_digit and 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints the last digit of a number
+ *
+ * @n: formal parameter (variable) that receives the value of the argument
+ *
+ * Return: last digit of the number
+ */
+
+int print_last_digit(int n)
+{
+	int d;
+	if (n >= 0)
+	{
+		d = n % 10;
+		_putchar(d + '0');
+		return (d);
+	}
+	else
+	{
+		n = 0 - n;
+		d = n % 10;
+		_putchar(d + '0');
+		return (d);
+	}
+}
