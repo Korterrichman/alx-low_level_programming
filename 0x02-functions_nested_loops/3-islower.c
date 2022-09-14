91cc488ded5f16c66abc300e5d7f81669f5ec005
Korter Richman <korterrichman@yahoo.com>
2022-09-14 01:19:14 -0700

    add 3-main.c, 3-islower and 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * Description: function that checks for lowercase character
+ * @c - formal parameter that accepts the value of the argument
+ * Return: 1 if c is lowercase and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
