e5b899d8dceeca4f39748718126db42c7bd45556
Korter Richman <korterrichman@yahoo.com>
2022-09-15 10:05:59 -0700

    add 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	/* local variable declaration */
+	int a = 0;
+	int b = 1;
+	int c, i;
+
+	for (i = 0; i < 50; i++)
+	{
+		c = a + b;
+		printf("%d, ", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+}
