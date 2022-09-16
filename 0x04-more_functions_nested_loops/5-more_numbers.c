3182636d7f96a4ed1f4ad762abf089d18a98249f
Korter Richman <korterrichman@yahoo.com>
2022-09-16 16:48:05 -0700

    add 5-main.c, 5-more_numbers and 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints the numbers
+ * from 0 to 14, followed by a new line
+ *
+ * Return: void (Nothing)
+ */
+
+void more_numbers(void)
+{
+	/* local variable declaration */
+	int n, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		n = 0;
+		while (n <= 14)
+			{
+				if (n >= 10)
+					_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				n++;
+			}
+		_putchar('\n');
+	}
+}
