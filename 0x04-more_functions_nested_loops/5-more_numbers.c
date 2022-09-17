b3419b82f0368d54104d5efb667e8298228ed403
Korter Richman <korterrichman@yahoo.com>
2022-09-16 17:01:06 -0700

    update 5-more_numbers and 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * more_numbers - function that prints the numbers
- * from 0 to 14, followed by a new line
+ * from 0 to 14, followed by a new line (ten times)
  *
  * Return: void (Nothing)
  */
