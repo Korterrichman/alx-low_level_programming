b7b4a4e9f101fa37016ba0358bfc8374bbe203f0
Korter Richman <korterrichman@yahoo.com>
2022-09-21 14:11:26 +0000

    add 1-main.c,  1-strncat and  1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+/**
+ * _strncat - function that concatenates two strings using at most n bytes from src
+ * @dest: formal parameter that receives the pointer of the first string
+ * @src: formal parameter that receives the pointer of the string to be added
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0, i = 0;
+
+	while (*(dest + len) != '\0')
+		len++;
+	while (*(src + i) != '\0' && i < n)
+	{
+		*(dest + len) = *(src + i);
+		len++;
+		i++;
+	}
+	return (dest);
+}
