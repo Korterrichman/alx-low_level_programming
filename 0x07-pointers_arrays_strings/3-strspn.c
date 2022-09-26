0e9e280efd57f2f34977b2ea22d90a1f863c586c
Korter Richman <korterrichman@yahoo.com>
2022-09-26 20:32:49 +0000

    add 2-strchr.c and 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @s: string to be checked
+ * @accept: accepted bytes in the initial segment of the string
+ * Return:  number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	/* local variable declaration */
+	int a, b, c;
+	unsigned int i;
+
+	for (a = 0; accept[a] != 0; a++)
+		;
+	i = 0;
+	for (b = 0; s[b] != 0; b++)
+	{
+		for (c = 0; c < b; c++)
+		{
+			if (s[b] == accept[c])
+			{
+				i++;
+				break;
+			}
+		}
+		if (b >= i)
+			return (i);
+	}
+	return (i);
+}
