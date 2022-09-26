f2651323a72f15137aae9de38594c7256f45cbe7
Korter Richman <korterrichman@yahoo.com>
2022-09-26 17:45:45 +0000

    update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,10 +10,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	/* local variable declaration */
-	int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
+	while (n)
+		s[n - 1] = b;
 	return (s);
 }
