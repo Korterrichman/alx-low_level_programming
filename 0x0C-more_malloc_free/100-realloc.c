c4d4ae3df6219a94b89f234f4217a7c1b57a3cb7
Korter Richman <korterrichman@yahoo.com>
2022-10-06 22:19:32 +0100

    add 100-realloc.c and update main.h

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+/**
+ * _realloc - function that reallocates a memory block using malloc and free
+ * @ptr: pointer to the former array
+ * @old_size: size of the former memory block
+ * @new_size: size of the new memory block to be allocated
+ * Return: pointer to the reallocated memory block on SUCCESS
+ * NULL on FAILURE
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int i;
+	char *tmp, *clone;
+	void *realloc;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+	clone = ptr;
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+
+	tmp = malloc(new_size);
+	if (tmp == NULL)
+		return (NULL);
+	for (i = 0; i < old_size && i < new_size; i++)
+		tmp[i] = clone[i];
+	realloc = tmp;
+	free(ptr);
+
+	return (realloc);
+}
